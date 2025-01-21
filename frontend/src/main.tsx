a56c029dcc8b197c8894c389b6db0c64b61beb5f
adelppi <adelppi.baig@gmail.com>
2025-01-21 13:55:25 +0900

    first commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,9 @@ import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-   <StrictMode>
+   // <StrictMode>
       <Provider>
          <App />
       </Provider>
-   </StrictMode>
+   // </StrictMode>
 );

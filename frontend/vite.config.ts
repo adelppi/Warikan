84d8b867987cadae374f582694bd9fd16911be45
adelppi <adelppi.baig@gmail.com>
2025-02-04 14:36:03 +0900

    first-commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,5 +7,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
    plugins: [react(), tsconfigPaths()],
    base: "/Warikan/",
-   build: { outDir: "./docs" },
+   build: { outDir: "../docs" },
 });
